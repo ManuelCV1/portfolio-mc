25ffd083bd5cf6c7202ea4dc2936c1af8434c891
ManuelCV1 <manuelcabrera.cv@gmail.com>
2023-03-15 16:39:45 -0400

    anchor-ref

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,9 +4,9 @@ export function Navbar() {
   return (
     <div className={style.navbarContainer}>
     <nav className={style.navbar}>
-      <a href="#" className={style.anchor}>Info</a>
-      <a href="#" className={style.anchor}>Proyectos</a>
-      <a href="#" className={style.anchor}>Contacto</a>
+      <span  className={style.anchor}>Info</span>
+      <span  className={style.anchor}>Proyectos</span>
+      <span  className={style.anchor}>Contacto</span>
     </nav>
     </div>
   );
