e0aba07895d2e80b655e42e809628527abea8fcd
ManuelCV1 <manuelcabrera.cv@gmail.com>
2023-03-29 12:26:17 -0400

    new-update

--- src/Proyects.jsx
+++ src/Proyects.jsx
@@ -5,7 +5,7 @@ export function Proyects({proyectsInfo}) {
 
     return (
         <div className={styles.proyectsContainer} id="Proyects">
-            <h1 className={styles.tituloPrincipal}>Proyectos Recientes <span role="img" aria-label="desktop">🖥️</span></h1>
+            <h1 className={styles.tituloPrincipal}>Proyectos Recientes <span className={styles.emojiTitle} role="img" aria-label="desktop">🖥️</span></h1>
             <ul className={styles.grid}>
                 {proyectsInfo.map( (pad,id) => <li key={id} className={styles.proyectTarget}><a href={pad.urlLink} target="_blank" rel="noreferrer"><img src={pad.Img} className={styles.imagenesProyects} alt="foto de proyecto"/></a><p className={styles.titleDescription}>{pad.proyectName}</p><p className={styles.appDescription}>{pad.proyectDescription}</p></li> )}
             </ul>
