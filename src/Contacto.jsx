7d29915be9508ddd82bb83ce462586d588b61a52
ManuelCV1 <manuelcabrera.cv@gmail.com>
2023-05-20 21:14:02 -0400

    update-responsive

--- src/Contacto.jsx
+++ src/Contacto.jsx
@@ -1,17 +1,43 @@
-import style from "./Contacto.module.css"
-import whatsappIcon from "./assets/icons/icons8-whatsapp-96.png"
-import GithubIcon from "./assets/icons/icons8-repositorio-96.png"
-import Correo from "./assets/icons/icons8-correo-electrónico-96.png"
+import style from "./Contacto.module.css";
+import whatsappIcon from "./assets/icons/icons8-whatsapp-96.png";
+import GithubIcon from "./assets/icons/icons8-repositorio-96.png";
+import Correo from "./assets/icons/icons8-correo-electrónico-96.png";
 
 export function Contacto() {
-    return (
-        <div className={style.contactContainer} id="Contact">
-            <h1 className={style.mainTitle}>Contáctame <span role="img" aria-label="mail">✉️</span></h1>
-            <div className={style.socialIconsContainer}>
-            <a href="https://wa.me/584242816455" target="_blank" rel="noreferrer"><img className={style.socialIcon} src={whatsappIcon} alt="+58-424-2816455" title="+58-424-2816455"/></a>
-            <a href="https://github.com/ManuelCV1" target="_blank" rel="noreferrer"><img className={style.socialIcon} src={GithubIcon} alt="https://github.com/ManuelCV1" title="https://github.com/ManuelCV1"/></a>
-            <a href="mailto:manuelcabrera.cv@gmail.com"><img src={Correo} className={style.socialIcon}alt="manuelcabrera.cv@gmail.com" title="manuelcabrera.cv@gmail.com"/></a>
-            </div>
-        </div>
-    );
+  return (
+    <div className={style.contactContainer} id="Contact">
+      <h1 className={style.mainTitle}>
+        Contáctame{" "}
+        <span role="img" aria-label="mail">
+          ✉️
+        </span>
+      </h1>
+      <div className={style.socialIconsContainer}>
+        <a href="https://wa.me/584242816455" target="_blank" rel="noreferrer">
+          <img
+            className={style.socialIcon}
+            src={whatsappIcon}
+            alt="+58-424-2816455"
+            title="+58-424-2816455"
+          />
+        </a>
+        <a href="https://github.com/ManuelCV1" target="_blank" rel="noreferrer">
+          <img
+            className={style.socialIcon}
+            src={GithubIcon}
+            alt="https://github.com/ManuelCV1"
+            title="https://github.com/ManuelCV1"
+          />
+        </a>
+        <a href="mailto:manuelcabrera.cv@gmail.com">
+          <img
+            src={Correo}
+            className={style.socialIcon}
+            alt="manuelcabrera.cv@gmail.com"
+            title="manuelcabrera.cv@gmail.com"
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/Proyects.jsx
+++ src/Proyects.jsx
@@ -1,14 +1,29 @@
-import styles from "./Proyects.module.css"
+import styles from "./Proyects.module.css";
 
-
-export function Proyects({proyectsInfo}) {
-
-    return (
-        <div className={styles.proyectsContainer} id="Proyects">
-            <h1 className={styles.tituloPrincipal}>Proyectos Recientes <span className={styles.emojiTitle} role="img" aria-label="desktop">🖥️</span></h1>
-            <ul className={styles.grid}>
-                {proyectsInfo.map( (pad,id) => <li key={id} className={styles.proyectTarget}><a href={pad.urlLink} target="_blank" rel="noreferrer"><img src={pad.Img} className={styles.imagenesProyects} alt="foto de proyecto"/></a><p className={styles.titleDescription}>{pad.proyectName}</p><p className={styles.appDescription}>{pad.proyectDescription}</p></li> )}
-            </ul>
-        </div>
-    );
+export function Proyects({ proyectsInfo }) {
+  return (
+    <div className={styles.proyectsContainer} id="Proyects">
+      <h1 className={styles.tituloPrincipal}>
+        Proyectos Recientes{" "}
+        <span className={styles.emojiTitle} role="img" aria-label="desktop">
+          🖥️
+        </span>
+      </h1>
+      <ul className={styles.grid}>
+        {proyectsInfo.map((pad, id) => (
+          <li key={id} className={styles.proyectTarget}>
+            <a href={pad.urlLink} target="_blank" rel="noreferrer">
+              <img
+                src={pad.Img}
+                className={styles.imagenesProyects}
+                alt="foto de proyecto"
+              />
+            </a>
+            <p className={styles.titleDescription}>{pad.proyectName}</p>
+            <p className={styles.appDescription}>{pad.proyectDescription}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
